0040999f6d929be00e7d8fd9c8d62989151cca5d
Sergey Sova <i.am@lestad.net>
2017-06-05 16:57:48 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const fingerprint = require('fingerprintjs2')
+const axios = require('axios')
+
+global.navigator = {}
+global.screen = {}
+global.window = {}
+global.document = {}
+
+let i = 0
+
+const fp = () => new Promise(resolve => resolve(Math.random().toString(36).substr(2)))
+
+async function main() {
+  const req = Array.from({ length: 10000 }, () => 'react')
+
+  for (let vote of req) {
+    const fingerprint = await fp()
+    const l = await axios.post('https://ng2-vs-react.herokuapp.com/vote', { vote }, {
+      headers: {
+        fingerprint,
+        Accept: 'application/json; charset=utf-8'
+      }
+    })
+    console.log(l, ++i)
+  }
+}
+
+
+main()
