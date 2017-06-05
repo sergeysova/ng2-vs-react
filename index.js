d8c322ed0aa08a9974fc2c0954ea50fc0c46ac3d
Sergey Sova <i.am@lestad.net>
2017-06-05 17:02:13 +0300

    Speed up

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ global.document = {}
 
 let i = 0
 
-const fp = () => new Promise(resolve => resolve(Math.random().toString(36).substr(2)))
+const fp = () => Math.random().toString(36).substr(2)
 
 async function main() {
   const req = Array.from({ length: 10000 }, () => 'react')
 
   for (let vote of req) {
-    const fingerprint = await fp()
+    const fingerprint = fp()
     const l = await axios.post('https://ng2-vs-react.herokuapp.com/vote', { vote }, {
       headers: {
         fingerprint,
